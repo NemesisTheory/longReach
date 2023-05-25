99e4731bc51aa5fb1c4265b61c3342ef51bec018
NemesisTheory <89642216+NemesisTheory@users.noreply.github.com>
2023-05-25 15:46:28 +0700

    forgor erekir

--- scripts/main.js
+++ scripts/main.js
@@ -2,4 +2,8 @@ Events.on(ClientLoadEvent, b => {
   UnitTypes.alpha.buildRange = Number.MAX_VALUE;
   UnitTypes.beta.buildRange = Number.MAX_VALUE;
   UnitTypes.gamma.buildRange = Number.MAX_VALUE;
+  
+  UnitTypes.evoke.buildRange = Number.MAX_VALUE;
+  UnitTypes.incite.buildRange = Number.MAX_VALUE;
+  UnitTypes.emanate.buildRange = Number.MAX_VALUE;
 });
